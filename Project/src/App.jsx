71596160435c158a2e8ff846855b31b2a2953f4d
mohammad <mmdmgh16@gmail.com>
2024-04-18 19:43:29 +0330

    Initial the project

---
+++ Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id='font' className='font-'>
+        khdfvkfhjk
+      </div>
+    </>
+  )
+}
+
+export default App
